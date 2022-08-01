913841ee1cef7d7b951a5fd0b1efadfbb1e6528c
박가연 <gy.park@newen.co.kr>
2022-08-01 16:20:12 +0900

    readonly properties

--- example.js
+++ example.js
@@ -1,10 +1,12 @@
 "use strict";
 class Person {
-    constructor(name, age) {
-        this.name = name;
+    constructor(_name, age) {
+        this._name = _name;
         this.age = age;
+        this.name = "na";
+        this.country = "korea"; //constructor >> readonly 초기화가능 
     }
 }
 const p1 = new Person("na", 3); //디폴트생성자 (인자가없는 생성자)
-console.log(p1);
-console.log(p1);
+console.log(p1.name); //get을 하는 함수 
+console.log(p1.name);
--- example.ts
+++ example.ts
@@ -1,20 +1,21 @@
 class Person {
+    public readonly name: string = "na";
+    private readonly country :string ;
+
     constructor(public _name: string, private age: number) {
+
+        this.country="korea";//constructor >> readonly 초기화가능 
     }
 
-    get name() {//
-        return this._name+"park";
-    }
-
-    // set name(n:string){
-    //     this._name= n;
-
+    // hello(){
+    //     this.country = "China";//readonly 라 갑변경 안됨
     // }
+
 }
 
 const p1 = new Person("na", 3);//디폴트생성자 (인자가없는 생성자)
 console.log(p1.name)//get을 하는 함수 
-//p1.name = "me";//set을 하는 함수 
 
 
+console.log(p1.name)
 
